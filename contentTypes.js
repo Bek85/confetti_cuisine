0635ad196489e8abd53b30035c42ec312285d8c8
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-03-18 16:05:28 +0500

    create content types for use in responses

--- contentTypes.js
+++ contentTypes.js
@@ -1,1 +1,21 @@
-"use strict";
+'use strict';
+module.exports = {
+  html: {
+    'Content-Type': 'text/html',
+  },
+  text: {
+    'Content-Type': 'text/plain',
+  },
+  js: {
+    'Content-Type': 'text/js',
+  },
+  jpg: {
+    'Content-Type': 'image/jpg',
+  },
+  png: {
+    'Content-Type': 'image/png',
+  },
+  css: {
+    'Content-Type': 'text/css',
+  },
+};
