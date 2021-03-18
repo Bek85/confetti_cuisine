8e3ff8a6349828258b0cdd05a591016b9ed40517
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-03-18 16:49:41 +0500

    create routes object, handle, get and post functions to handle app routing logic

--- router.js
+++ router.js
@@ -1,1 +1,26 @@
-"use strict";
+'use strict';
+const { StatusCodes } = require('http-status-codes');
+const contentTypes = require('./contentTypes');
+const utils = require('./utils');
+
+const routes = {
+  GET: {},
+  POST: {},
+};
+
+exports.handle = (req, res) => {
+  try {
+    routes[req.method][req.url](req, res);
+  } catch (e) {
+    res.writeHead(StatusCodes.OK, contentTypes.html);
+    utils.getFile('views/error.html', res);
+  }
+};
+
+exports.get = (url, action) => {
+  routes['GET'][url] = action;
+};
+
+exports.post = (url, action) => {
+  routes['POST'][url] = action;
+};
