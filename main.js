6287b0799c09498f501db5fb65adace5823e53f7
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-03-18 16:01:49 +0500

    create initial contents of main.js with required modules

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-"use strict";
+'use strict';
+const port = 3000;
+const http = require('http');
+const { StatusCodes } = require('http-status-codes');
+const router = require('./router');
+const contentTypes = require('./contentTypes');
+const utils = require('./utils');
