80016a78c71384b407437e6f728a675bbf00a54f
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-03-18 16:09:27 +0500

    create a function to search for a file at the provided path with error checking

--- utils.js
+++ utils.js
@@ -1,1 +1,16 @@
-"use strict";
+'use strict';
+const fs = require('fs');
+const { StatusCodes } = require('http-status-codes');
+const contentTypes = require('./contentTypes');
+
+module.exports = {
+  getFile: (file, res) => {
+    fs.readFile(`./${file}`, (err, data) => {
+      if (err) {
+        res.writeHead(StatusCodes.INTERNAL_SERVER_ERROR, contentTypes.html);
+        res.end('There was an error serving content!');
+      }
+      res.end(data);
+    });
+  },
+};
